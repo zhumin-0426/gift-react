c43a8b215e6d62bb24346c911206a171d3689484
jack <1404883943@qq.com>
2021-01-08 15:54:39 +0800

    axios封装

--- src/common/js/axios.js
+++ src/common/js/axios.js
@@ -1,3 +1,46 @@
-function Axios() {
+import axios from 'axios';
 
+let getAxios = (url, params) => {
+    // 基础路径
+    axios.defaults.baseURL = "http://127.0.0.1:8888/admin";
+    return new Promise((resolve, reject) => {
+        axios({
+            url: url,
+            method: 'get',
+            params: params,
+        }).then(response => {
+            if (response.status == 200) {
+                resolve(response);
+            } else {
+                throw new Error('Response data error')
+            }
+        }).catch(error => {
+            reject(error)
+        })
+    })
+}
+
+let postAxios = (url, params) => {
+    // 基础路径
+    axios.defaults.baseURL = "http://127.0.0.1:8888/admin";
+    return new Promise((resolve, reject) => {
+        axios({
+            url: url,
+            method: 'post',
+            params: params,
+        }).then(response => {
+            if (response.status == 200) {
+                resolve(response);
+            } else {
+                throw new Error('Response data error')
+            }
+        }).catch(error => {
+            reject(error)
+        })
+    })
+}
+
+export default {
+    getAxios,
+    postAxios
 }
