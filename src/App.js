38b1383628f48454c7fb8472d975f8b1a2305a52
zhumin <18820854754@139.com>
2021-02-23 20:59:34 +0800

    react

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import './App.less';
 import SideBar from './components/sidebar';
 import Header from './components/header';
 import Footer from './components/footer';
-// 子组件
-import Login from './pages/login';
-// import System from './pages/system';
-// import Goods from './pages/goods';
-// import Order from './pages/order';
-// import Users from './pages/users';
-// import PageDesign from './pages/pageDesign';
-// import Demo from './pages/demo';
 // 路由表
 import {routerTable} from './common/js/roterTable';
 // 路由守卫组件
@@ -38,19 +30,12 @@ class App extends React.Component {
       <div className="app">
         <Router>
           <Switch>
-            <Route path="/login" exact component={Login}></Route>
             <div className="wrapper">
               <Header getChildDatas={this.getChildDatas} />
               <div className="content-wrapper">
                 <SideBar isCollapsed={this.state.isCollapsed} />
                 <div className="content w100 pd-30">
                   <Switch>
-                    {/* <Route path="/system" component={System} />
-                    <Route path="/pageDesign" component={PageDesign} />
-                    <Route path="/goods" component={Goods} />
-                    <Route path="/order" component={Order} />
-                    <Route path="/users" component={Users} />
-                    <Route path="/demo" component={Demo} /> */}
                     <FrontendAuth routerConfig={routerTable} />
                   </Switch>
                   <Footer />
--- src/common/js/roterTable.js
+++ src/common/js/roterTable.js
@@ -7,7 +7,7 @@ import PageDesign from '../../pages/pageDesign';
 import Demo from '../../pages/demo';
 // auth 是否需要登录
 export let routerTable = [
-    { path: "/login", name: "login", component: Login, auth: true },
+    { path: "/login", name: "login", component: Login },
     { path: "/system", name: "system", component: System, auth: true },
     { path: "/goods", name: "goods", component: Goods, auth: true },
     { path: "/order", name: "order", component: Order, auth: true },
